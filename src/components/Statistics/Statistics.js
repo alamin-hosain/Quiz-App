93480c57ed13c6f4bbc22258fddaac07f65ea056
Hossain Alamin <hossainalamin@Hossains-MacBook-Pro.local>
2022-10-11 11:46:08 +0600

    finish statistic

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { QuizContext } from '../Root';
 
 const Statistics = () => {
+
+    const quiz = useContext(QuizContext);
+
     return (
-        <div>
-            <h1>Statistics</h1>
+
+        <div className='flex justify-center items-center mt-56 mb-80 '>
+
+            <BarChart
+                width={590}
+                height={420}
+                data={quiz}
+                margin={{
+                    top: 5,
+                    right: 50,
+                    left: 50,
+                    bottom: 5,
+                }}
+            >
+
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="id" fill="#8884d8" />
+                <Bar dataKey="total" fill="#82ca9d" />
+            </BarChart>
+
         </div>
+
     );
 };
 
